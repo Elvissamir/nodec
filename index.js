6d218ed3fb8ba95c0114321175368cae9a46fbc5
Elvis <elvissamir18@gmail.com>
2022-02-07 08:15:40 -0400

    Added createCourse and getCourses functions.

--- index.js
+++ index.js
@@ -11,9 +11,43 @@ const dbDebugger = require('debug')('app:db')
 const morgan = require('morgan')
 
 // DB 
-moongose.connect('mongodb://localhost/school_db')
+moongose.connect('mongodb://localhost/playground')
         .then(() => console.log('(Mongoose) connected to mongodb...'))
         .catch(err => console.log('(Mongoose) could not connect to mongodb...', err))
+
+const courseSchema = new moongose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPusblished: Boolean
+})
+
+const Course = moongose.model('Course', courseSchema)
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Angular',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
+        isPusblished: true
+    })
+    
+    const result = await course.save()
+    console.log(result)
+}
+
+async function getCourse() {
+    const courses = await Course
+        .find({ author: 'Mosh', isPusblished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 })
+
+    console.log(courses)
+}
+
+getCourse()
 
 // Routes
 const courses = require('./routes/courses')
